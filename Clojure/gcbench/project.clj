a61caa5925a837a2db72c5c29ca19cd696323466
jeff murphy <jcmurphy@jeffmurphy.org>
2016-09-25 19:11:48 -0400

    add command line argument parsing

--- Clojure/gcbench/project.clj
+++ Clojure/gcbench/project.clj
@@ -1,5 +1,5 @@
 (defproject gcbench "1.0.0-SNAPSHOT"
   :description "ubmltongroup clojure gcbench port"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"][org.clojure/tools.cli "0.3.5"]]
   :main gcbench.core
 )
--- Clojure/gcbench/src/gcbench/core.clj
+++ Clojure/gcbench/src/gcbench/core.clj
@@ -1,4 +1,7 @@
-(ns gcbench.core)
+(ns gcbench.core
+  (:require [clojure.string :as string] [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
+
 
 ;; http://macromancy.com/2014/04/09/data-structures-clojure-trees.html
 
@@ -49,10 +52,70 @@
         ;; if both a non-nil left and `k` is less than key
         (and (lt? k key) left) (.lookup left k)))))
 
-
-(defn -main []
-  "I can say 'Hello World'."
-  (println "Hello, World!"))
-
 (defn bst [& [k v]] (Node. k v nil nil))
 
+;; ./yourprogram -ct <#> -ci <#> -cs <#> -gt <#> -ts <#> -m <#> -s -h
+
+(def cli-options
+  ;; An option with a required argument
+  [["-ct" "--compute-threads NUM" "Compute Threads"
+    :default 1
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-cd" "--compute-depth NUM" "Compute Depth"
+    :default 37
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 1 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-ci" "--compute-iterations NUM" "Compute Iterations"
+    :default 10
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 1 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-cs" "--compute-sleep NUM" "Compute Sleep"
+    :default 1
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-gt" "--gc-threads NUM" "GC Threads"
+    :default 1
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-td" "--tree-depth NUM" "Maximum tree depth to allocate"
+    :default 50
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 1 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-m" "--maxheap NUM" "Maximum heap to allocate (in MB)"
+    :default 4
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ;; A non-idempotent option
+   ["-s" "--gc-stats" "Print GC stats"]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["Clojure MT/GC Bench"
+        ""
+        "Usage: lein run -- [options]"
+        ""
+        "Options:"
+        options-summary
+       ]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+
+(defn -main [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not= (count arguments) 1) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+  (println "Hello, World!")))
+  
