907d3ca92a28c7e0f306fd65cecf8c6b4fbec326
bhargavshivkumar <bhargavs@buffalo.edu>
2017-05-30 13:08:58 -0400

    clojure

---
+++ Fragmentation_tolerance/fraggerclj/project.clj
@@ -0,0 +1,8 @@
+(defproject fraggerclj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main fraggerclj.core/foo
+  :jvm-opts ["-Xmx55297k"]
+  :dependencies [[org.clojure/clojure "1.8.0"][im.chit/ova "1.0.1"]])
---
+++ Fragmentation_tolerance/fraggerclj/src/fraggerclj/core.clj
@@ -0,0 +1,43 @@
+(ns fraggerclj.core
+  (:gen-class))
+
+(use 'ova.core)
+
+(defn fragmentArray
+  [arr]
+(<< (!! arr even? nil))
+)
+
+(defn allocateArray 
+  [kArraySize]
+  ( ova (range kArraySize))
+)
+
+(defn allocateArray2 
+  [kArraySize]
+  (def sz (* kArraySize 3))
+  ( ova (range sz (+ sz kArraySize) ))
+)
+(defn traverseArray
+  [arr]
+  (<< (map! arr (fn [n] (+ n 1))))
+)
+;;(defn allocateArray 
+;;  [kArraySize]
+;;  (def arr (ova (range kArraySize)))
+;;  (fragmentArray arr)
+;;  (println arr)
+;;  )
+
+(defn foo
+  "I don't do a whole lot."
+  [& args]
+  (def kArraySize 100000)
+  (traverseArray (allocateArray kArraySize))
+  (println "allocated large array")
+  (println (traverseArray (allocateArray2 (/ kArraySize 2))))
+  (println "Allocated second array")
+  ;;(time (allocateArray (/ kArraySize 2)))
+ ;; (println  "Hello, World!")
+  )
+
