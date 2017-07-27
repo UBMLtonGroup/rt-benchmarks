e8428a5ee5b00d817fb4e2b6034f8a9fda42bfe5
bhargavshivkumar <bhargavs@buffalo.edu>
2017-07-27 15:31:29 -0400

    clojure jupyter

--- Fragmentation_tolerance/fraggerclj/runclj.py
+++ Fragmentation_tolerance/fraggerclj/runclj.py
@@ -3,11 +3,12 @@ import commands
 import subprocess
 import matplotlib.pyplot as plt
 import re
+import csv
 
 def main():
     start = 30 * 1024 * 1024 #524288000
     increment = 2 *1024 *1024
-    values = [x for x in range(start,(start + (increment * 50)),increment)]
+    values = [x for x in range(start,(start + (increment * 10)),increment)]
     res =[]
     print("Starting script\n")
     for i in values:
@@ -19,13 +20,20 @@ def main():
         res.append(result)
     print("Plotting graph\n")
     res = [re.findall("\d+\.\d+",x) for x in res]
+    res = [float(x) for [x] in res]
     print(res)
     values = [x/1000000 for x in values]
-    plt.plot(values,res)
-    plt.xlabel('Heap size (MB)')
-    plt.ylabel('Time (ms)')
-    plt.title('Clojure Fragmentation tolerance \n'+'Start size: '+str(start/(1024*1024))+' MB\n'+'Increment: '+str(increment/(1024*1024))+' MB')
-    plt.show()
+    c = zip(values,res)
+    with open("cljfrag.txt",'wb') as myFile:
+        wr = csv.writer(myFile, delimiter=',')
+        for a,b in c:
+            d =[a,b]
+            wr.writerow(d)
+    #plt.plot(values,res)
+    #plt.xlabel('Heap size (MB)')
+    #plt.ylabel('Time (ms)')
+    #plt.title('Clojure Fragmentation tolerance \n'+'Start size: '+str(start/(1024*1024))+' MB\n'+'Increment: '+str(increment/(1024*1024))+' MB')
+    #plt.show()
 
 
 main()
