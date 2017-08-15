44ba3c8ab0a62b4aa43bd32f0a485a6962dce18c
bhargavshivkumar <bhargavs@buffalo.edu>
2017-08-15 17:52:07 -0400

    edits

--- Fragmentation_tolerance/scala_small_heap/runtest.py
+++ Fragmentation_tolerance/scala_small_heap/runtest.py
@@ -26,43 +26,14 @@ def run(st,inc):
     free = [int(y)/1000000 for x,y in a]
     values = [x/1000000 for x in values]
     d = zip(values,res,free)
-    with open("scalafrag.txt",'wb') as myFile:
+    name = "scala_"+str(inc)+"k.txt"
+    with open(name,'wb') as myFile:
         wr  = csv.writer(myFile, delimiter=',')
         for a,b,c in d:
             e =[a,b ,c]
             wr.writerow(e)
 
-    heap, time , free = np.loadtxt('scalafrag.txt', delimiter=',', unpack=True)
-
-    fig,ax1 = plt.subplots(dpi=120, figsize=(7,7))
-    ax2 = plt.twinx()
-
-    ax1.set_ylabel("Allocation time ($ms$)",color = 'blue')
-    ax1.set_xlabel("Initial heap size ($MB$)")
-
-    ax2.set_ylabel("Free space on heap ($MB$)",color = 'green')
-    ax2.set_xlabel("Initial heap size ($MB$)")
-
-
-    p1,= ax1.plot(heap,time, label='Time taken to allocate large array')
-    p2,= ax2.plot(heap,free , label='Free space on heap' ,color = 'green')
-
-
-    plt.title('Scala Fragmentation tolerance')
-
-    from matplotlib.font_manager import FontProperties
-    fontP = FontProperties()
-    fontP.set_size('small')
-
-
-    lines =[p1,p2]
-
-    plt.legend(lines, [l.get_label() for l in lines],prop = fontP ,loc =9)
-
-    name = "scala_"+str(inc)+"k.png"
-
-    plt.savefig(name)
-
+   
 
 def main():
     for i in range(100,1000,50):
