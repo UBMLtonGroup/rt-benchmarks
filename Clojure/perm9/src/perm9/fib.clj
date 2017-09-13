097112db1d19b9926a6a41be1ae8da9954e4a93e
jeff murphy <jcmurphy@jeffmurphy.org>
2017-09-13 19:56:51 -0400

    type

--- Clojure/perm9/src/perm9/fib.clj
+++ Clojure/perm9/src/perm9/fib.clj
@@ -14,7 +14,7 @@
   (loop [res [0 1]]
           (if (>= (count res) max)
             res
-            (recur (conj res (+' (inc (last res)) (dec (last (butlast res))))))))
+            (recur (conj res (+' (inc (last res)) (dec (last (butlast res)))))))))
 
 
 ;; 
